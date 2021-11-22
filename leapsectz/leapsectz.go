712d476336893e46a489db62320218fba928efb5
Vadim Fedorenko <vadfed@fb.com>
2021-11-22 15:31:21 +0000

    leapseconds: add parser for version 2 files
    The latest file are going in version 2 with uint64 type used to store
    all the timestamp information. Add parser for that version.
    Also some changes to leap second structures - number of seconds is
    signed value and should be read as signed. And also some optimisation
    of skipping data without actual memory allocation. Tests are added

--- leapsectz/leapsectz.go
+++ leapsectz/leapsectz.go
@@ -23,6 +23,7 @@ import (
 	"encoding/binary"
 	"errors"
 	"io"
+	"io/ioutil"
 	"os"
 	"time"
 )
@@ -30,16 +31,17 @@ import (
 const file = "/usr/share/zoneinfo/right/UTC"
 
 var errBadData = errors.New("malformed time zone information")
+var errBadVersion = errors.New("version in file is not supported")
 
 // LeapSecond represents a leap second
 type LeapSecond struct {
-	Tleap uint32
-	Nleap uint32
+	Tleap uint64
+	Nleap int32
 }
 
 // Time returns when the leap second event occurs
 func (l LeapSecond) Time() time.Time {
-	return time.Unix(int64(l.Tleap-l.Nleap+1), 0)
+	return time.Unix(int64(l.Tleap-uint64(l.Nleap)+1), 0)
 }
 
 // Parse returns the list of leap seconds
@@ -50,72 +52,114 @@ func Parse() ([]LeapSecond, error) {
 	}
 	defer f.Close()
 
-	return parse(f)
+	return parseVx(f)
+
 }
 
-func parse(r io.Reader) ([]LeapSecond, error) {
-	// 4-byte magic "TZif"
-	magic := make([]byte, 4)
-	if _, _ = r.Read(magic); string(magic) != "TZif" {
-		return nil, errBadData
-	}
-
-	// 1-byte version, then 15 bytes of padding
-	p := make([]byte, 16)
-	if n, _ := r.Read(p); n != 16 {
-		return nil, errBadData
-	} else if p[0] != 0 && p[0] != '2' && p[0] != '3' {
-		return nil, errBadData
-	}
-
-	// six big-endian 32-bit integers:
-	//	number of UTC/local indicators
-	//	number of standard/wall indicators
-	//	number of leap seconds
-	//	number of transition times
-	//	number of local time zones
-	//	number of characters of time zone abbrev strings
-	const (
-		NUTCLocal = iota
-		NStdWall
-		NLeap
-		NTime
-		NZone
-		NChar
-	)
-	var n [6]int
-	for i := 0; i < 6; i++ {
-		var nn uint32
-		err := binary.Read(r, binary.BigEndian, &nn)
-		if err != nil {
-			return nil, err
-		}
-		n[i] = int(nn)
-	}
-
-	// skip uninteresting data:
-	//  tzh_timecnt (char [4])s  coded transition times a la time(2)
-	//  tzh_timecnt (unsigned char)s types of local time starting at above
-	//  tzh_typecnt repetitions of
-	//   one (char [4])  coded UT offset in seconds
-	//   one (unsigned char) used to set tm_isdst
-	//   one (unsigned char) that's an abbreviation list index
-	//  tzh_charcnt (char)s  '\0'-terminated zone abbreviations
-	skip := n[NTime]*5 + n[NZone]*6 + n[NChar]
-	data := make([]byte, skip)
-	if n, _ := r.Read(data); n != skip {
-		return nil, errBadData
-	}
-
+func parseVx(r io.Reader) ([]LeapSecond, error) {
 	var ret []LeapSecond
-	for i := 0; i < int(n[NLeap]); i++ {
-		l := LeapSecond{}
-		err := binary.Read(r, binary.BigEndian, &l)
-		if err != nil {
-			return nil, err
+	var v byte
+	for v = 0; v < 2; v++ {
+		// 4-byte magic "TZif"
+		magic := make([]byte, 4)
+		if _, _ = r.Read(magic); string(magic) != "TZif" {
+			return nil, errBadData
 		}
-		ret = append(ret, l)
-	}
 
+		// 1-byte version, then 15 bytes of padding
+		var version byte
+		p := make([]byte, 16)
+		if n, _ := r.Read(p); n != 16 {
+			return nil, errBadData
+		}
+
+		version = p[0]
+		if version != 0 && version != '2' && version != '3' {
+			return nil, errBadVersion
+		}
+
+		if v > version {
+			return nil, errBadData
+		}
+		// six big-endian 32-bit integers:
+		//	number of UTC/local indicators
+		//	number of standard/wall indicators
+		//	number of leap seconds
+		//	number of transition times
+		//	number of local time zones
+		//	number of characters of time zone abbrev strings
+		const (
+			NUTCLocal = iota
+			NStdWall
+			NLeap
+			NTime
+			NZone
+			NChar
+		)
+		var n [6]int
+		for i := 0; i < 6; i++ {
+			var nn uint32
+			err := binary.Read(r, binary.BigEndian, &nn)
+			if err != nil {
+				return nil, err
+			}
+			n[i] = int(nn)
+		}
+
+		// skip uninteresting data:
+		//  tzh_timecnt (char [4] or char [8] for ver 2)s  coded transition times a la time(2)
+		//  tzh_timecnt (unsigned char)s types of local time starting at above
+		//  tzh_typecnt repetitions of
+		//   one (char [4])  coded UT offset in seconds
+		//   one (unsigned char) used to set tm_isdst
+		//   one (unsigned char) that's an abbreviation list index
+		//  tzh_charcnt (char)s  '\0'-terminated zone abbreviations
+		var skip int
+		if v == 0 {
+			skip = n[NTime]*5 + n[NZone]*6 + n[NChar]
+		} else {
+			skip = n[NTime]*9 + n[NZone]*6 + n[NChar]
+		}
+
+		// if it's first part of two parts file (version 2 or 3)
+		// then skip it completely
+		if v == 0 && version > 0 {
+			skip += n[NLeap]*8 + n[NUTCLocal] + n[NStdWall]
+		}
+
+		if n, _ := io.CopyN(ioutil.Discard, r, int64(skip)); n != int64(skip) {
+			return nil, errBadData
+		}
+
+		if v == 0 && version > 0 {
+			v++
+			continue
+		}
+
+		// calculate the amount of bytes to skip after reading leap seconds array
+		skip = n[NUTCLocal] + n[NStdWall]
+
+		for i := 0; i < int(n[NLeap]); i++ {
+			var l LeapSecond
+			if version == 0 {
+				lsv0 := []uint32{0, 0}
+				err := binary.Read(r, binary.BigEndian, &lsv0)
+				if err != nil {
+					return nil, err
+				}
+				l.Tleap = uint64(lsv0[0])
+				l.Nleap = int32(lsv0[1])
+			} else {
+				err := binary.Read(r, binary.BigEndian, &l)
+				if err != nil {
+					return nil, err
+				}
+			}
+			ret = append(ret, l)
+		}
+		// we need to skip the rest of the data
+		_, _ = io.CopyN(ioutil.Discard, r, int64(skip))
+		break
+	}
 	return ret, nil
 }
