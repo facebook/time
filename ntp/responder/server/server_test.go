ac5ca3850d1ef9cbb66a2d127caf3bddc3b10267
Oleg Obleukhov <leoleovich@fb.com>
2022-02-01 12:05:31 +0000

    [ntp/responder] improve test coverage

--- ntp/responder/server/server_test.go
+++ ntp/responder/server/server_test.go
@@ -18,14 +18,29 @@ package server
 
 import (
 	"encoding/binary"
+	"net"
 	"testing"
 	"time"
 
 	ntp "github.com/facebook/time/ntp/protocol"
+	"github.com/facebook/time/ntp/responder/checker"
+	"github.com/facebook/time/ntp/responder/stats"
+	"github.com/facebook/time/timestamp"
 	"github.com/stretchr/testify/require"
 )
 
 var ts = time.Unix(1585231321, 148166539)
+
+// Packet request. From ntpdate run
+var ntpRequest = &ntp.Packet{
+	Settings:       227,
+	Poll:           3,
+	Precision:      -6,
+	RootDelay:      65536,
+	RootDispersion: 65536,
+	TxTimeSec:      3794210679,
+	TxTimeFrac:     2718216404,
+}
 
 func TestFillStaticHeadersStratum(t *testing.T) {
 	stratum := 1
@@ -90,6 +105,48 @@ func TestGenerateResponsetss(t *testing.T) {
 	// Transmit ts must be current ts
 	require.Equal(t, nowSec, response.TxTimeSec)
 	require.Equal(t, nowFrac, response.TxTimeFrac)
+}
+
+func TestListener(t *testing.T) {
+	s := &Server{
+		Checker: &checker.SimpleChecker{
+			ExpectedListeners: 1,
+			ExpectedWorkers:   0,
+		},
+	}
+	go s.startListener(net.ParseIP("127.0.0.1"), 0)
+	time.Sleep(100 * time.Millisecond)
+
+	err := s.Checker.Check()
+	require.NoError(t, err)
+}
+
+func TestWorker(t *testing.T) {
+	s := &Server{
+		Checker: &checker.SimpleChecker{
+			ExpectedListeners: 0,
+			ExpectedWorkers:   1,
+		},
+		Stats: &stats.JSONStats{},
+		tasks: make(chan task),
+	}
+
+	// listen to incoming udp ntp.
+	conn, err := net.ListenUDP("udp", &net.UDPAddr{IP: net.ParseIP("127.0.0.1"), Port: 0})
+	require.NoError(t, err)
+	defer conn.Close()
+
+	// get connection file descriptor
+	connFd, err := timestamp.ConnFd(conn)
+	require.NoError(t, err)
+
+	sa := timestamp.IPToSockaddr(net.ParseIP("127.0.0.1"), 0)
+
+	go s.startWorker()
+	time.Sleep(100 * time.Millisecond)
+	err = s.Checker.Check()
+	require.NoError(t, err)
+	s.tasks <- task{connFd: connFd, addr: sa, received: time.Now(), request: ntpRequest, stats: &stats.JSONStats{}}
 }
 
 func Benchmark_generateResponse(b *testing.B) {
