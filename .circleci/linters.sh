957e75944c465973261f7420467ff29e2e6adc76
Pablo Mazzini <pmazzini@gmail.com>
2022-04-01 14:52:24 +0100

    [ci] use github actions #8

--- .circleci/linters.sh
+++ .circleci/linters.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bash
 set -exu
 
-# install golangci-lint as recommended on the project page
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin latest
-cd "${CIRCLE_WORKING_DIRECTORY}"
-golangci-lint run --enable deadcode --enable varcheck --enable staticcheck
-
-# check format
-if [ "$(go fmt ./... | wc -l)" -gt 0 ]; then
-    exit 1
-fi
-
 # check license headers
 # this needs to be run from the top level directory, because it uses
 # `git ls-files` under the hood.
@@ -19,9 +9,3 @@ go install github.com/u-root/u-root/tools/checklicenses
 cd "${CIRCLE_WORKING_DIRECTORY}"
 echo "[*] Running checklicenses"
 go run github.com/u-root/u-root/tools/checklicenses -c .circleci/config.json
-
-# Check if there are any spelling errors
-go get -v -u github.com/client9/misspell
-cd "${CIRCLE_WORKING_DIRECTORY}"
-echo "[*] Running misspell"
-go run github.com/client9/misspell/cmd/misspell -error ./
