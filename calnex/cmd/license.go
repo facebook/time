65062b7080ce8971b617734f6525f9ceb3db9fcd
Iqbal Hossain <iqbal@meta.com>
2023-02-21 10:58:30 -0800

    Command line interface for uploading license (#256)
    Summary:
    Pull Request resolved: https://github.com/facebook/time/pull/256
    
    Right now Calnex is using curl to upload the license file:
    `$ curl -k https://calnex03.rva1.fbinfra.net/api/option/load --data-binary "sentinel_multiport.lic"`
    
    Created a command line interface for Calnex and the API wrapper to upload the license file to the device. It can be achieved by passing the `license` argument to the calnex cli program.
    
    `buck run //ntp/calnex:calnex -- license --file /tmp/sentinel_multiport.lic --target calnex01.snc1.fbinfra.net --insecureTLS`
    
    It successfully uploads the license file to the device.
    
    Wrote unit tests as well for ensuring there are no functional errors in the code.
    
    Reviewed By: leoleovich
    
    Differential Revision: D43463892
    
    fbshipit-source-id: be409b39a7f97f72f5abad75d082e436d9d8bf7e

---
+++ calnex/cmd/license.go
@@ -0,0 +1,53 @@
+/*
+Copyright (c) Facebook, Inc. and its affiliates.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cmd
+
+import (
+	"github.com/facebook/time/calnex/api"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	RootCmd.AddCommand(licenseCmd)
+	licenseCmd.Flags().BoolVar(&insecureTLS, "insecureTLS", false, "Ignore TLS certificate errors")
+	licenseCmd.Flags().StringVar(&target, "target", "", "device to configure")
+	licenseCmd.Flags().StringVar(&source, "file", "", "license file path")
+
+	if err := licenseCmd.MarkFlagRequired("target"); err != nil {
+		log.Fatal(err)
+	}
+	if err := licenseCmd.MarkFlagRequired("file"); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func licenseFunc() error {
+	api := api.NewAPI(target, insecureTLS)
+	_, err := api.PushLicense(source)
+	return err
+}
+
+var licenseCmd = &cobra.Command{
+	Use:   "license",
+	Short: "install device license",
+	Run: func(cmd *cobra.Command, args []string) {
+		if err := licenseFunc(); err != nil {
+			log.Fatal(err)
+		}
+	},
+}
