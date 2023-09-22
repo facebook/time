bd6e7134b25b875c82456428075e89fb574ee0f5
Oleg Obleukhov <leoleovich@meta.com>
2023-09-22 13:45:38 -0700

    remove rate metrics (#297)
    Summary:
    Pull Request resolved: https://github.com/facebook/time/pull/297
    
    Almost every monitoring solution can calculate *rate* from *sum* over an X interval (just need to divide by X).
    
    Reviewed By: pmazzini
    
    Differential Revision: D49548841
    
    fbshipit-source-id: 4fb3716bc596fa8cd98cd8874b3e80d3a28df825

--- ptp/sptp/client/sysstats_test.go
+++ ptp/sptp/client/sysstats_test.go
@@ -25,7 +25,7 @@ import (
 )
 
 var expectedNonAggregateKeys = []string{"runtime.cpu.cgo_calls", "runtime.cpu.goroutines", "process.cpu_pct.avg.1", "runtime.mem.alloc", "runtime.mem.frees", "runtime.mem.gc.count", "runtime.mem.gc.last", "runtime.mem.gc.next", "runtime.mem.gc.pause", "runtime.mem.gc.pause_total", "runtime.mem.gc.sys", "runtime.mem.heap.alloc", "runtime.mem.heap.idle", "runtime.mem.heap.inuse", "runtime.mem.heap.objects", "runtime.mem.heap.released", "runtime.mem.heap.sys", "runtime.mem.lookups", "runtime.mem.malloc", "runtime.mem.othersys", "runtime.mem.stack.inuse", "runtime.mem.stack.mcache_inuse", "runtime.mem.stack.mcache_sys", "runtime.mem.stack.mspan_inuse", "runtime.mem.stack.mspan_sys", "runtime.mem.stack.sys", "runtime.mem.sys", "runtime.mem.total", "process.num_fds", "process.num_threads", "process.rss", "process.swap", "process.uptime", "process.vms"}
-var expectedAggKeys = []string{"runtime.mem.mallocs.rate.1", "runtime.mem.mallocs.sum.1", "runtime.mem.total_alloc.rate.1", "runtime.mem.total_alloc.sum.1", "runtime.gc.count.rate.1", "runtime.gc.count.sum.1", "runtime.gc.pause_ns.rate.1", "runtime.gc.pause_ns.sum.1", "runtime.lookups.rate.1", "runtime.lookups.sum.1", "runtime.mem.frees.rate.1", "runtime.mem.frees.sum.1"}
+var expectedAggKeys = []string{"runtime.mem.mallocs.sum.1", "runtime.mem.total_alloc.sum.1", "runtime.gc.count.sum.1", "runtime.gc.pause_ns.sum.1", "runtime.lookups.sum.1", "runtime.mem.frees.sum.1"}
 
 func TestSysStats(t *testing.T) {
 	stats := SysStats{}
@@ -46,11 +46,10 @@ func TestSysStats(t *testing.T) {
 func TestSetRate(t *testing.T) {
 	stats := make(map[string]uint64)
 	intervaltime := time.Second * time.Duration(5)
-	setRate("test", stats, 20, 1, intervaltime)
+	setDiff("test", stats, 20, 1, intervaltime)
 
 	expected := map[string]uint64{
-		"test.sum.5":  19,
-		"test.rate.5": 3,
+		"test.sum.5": 19,
 	}
 	require.Equal(t, expected, stats)
 }
