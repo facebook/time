facebook/time

commit e51723aa71749cd8aab61b03b68d8fe53392b8e7

use netip.Addr in sptp to reduce heap allocations (#370)
Summary:
Pull Request resolved: https://github.com/facebook/time/pull/370

new cool netip package provides better version of IP type, which is fixed length, can be passed by value, is comparable and thus can be used as map keys.
We save plenty of allocations by not converting IPs to strings to use as map keys.

Reviewed By: leoleovich, deathowl

Differential Revision: D58733846

cmd/ptpcheck/cmd/ptping.go
@@ -21,6 +21,7 @@ import (
 	"fmt"
 	"math/rand"
 	"net"
+	"net/netip"
 	"time"
 
 	"github.com/facebook/time/dscp"
@@ -65,7 +66,7 @@ func (t *timestamps) reset() {
 type ptping struct {
 	iface  string
 	dscp   int
-	target string
+	target netip.Addr
 
 	clockID   ptp.ClockIdentity
 	eventConn client.UDPConnWithTS
@@ -171,9 +172,13 @@ func (p *ptping) runReader() error {
 func ptpingRun(iface string, dscp int, server string, count int, timeout time.Duration) error {
 	var err error
 	p := &ptping{
-		iface:  iface,
-		dscp:   dscp,
-		target: server,
+		iface: iface,
+		dscp:  dscp,
+	}
+
+	p.target, err = netip.ParseAddr(server)
+	if err != nil {
+		return err
 	}
 
 	if err = p.init(); err != nil {

ptp/sptp/client/sptp.go
@@ -21,6 +21,7 @@ import (
 	"errors"
 	"fmt"
 	"net"
+	"net/netip"
 	"sync"
 	"time"
 
@@ -54,11 +55,11 @@ type SPTP struct {
 
 	clock Clock
 
-	bestGM string
+	bestGM netip.Addr
 
-	clients    map[string]*Client
-	priorities map[string]int
-	backoff    map[string]*backoff
+	clients    map[netip.Addr]*Client
+	priorities map[netip.Addr]int
+	backoff    map[netip.Addr]*backoff
 	lastTick   time.Time
 
 	clockID ptp.ClockIdentity
@@ -83,19 +84,21 @@ func NewSPTP(cfg *Config, stats StatsServer) (*SPTP, error) {
 }
 
 func (p *SPTP) initClients() error {
-	p.clients = map[string]*Client{}
-	p.priorities = map[string]int{}
-	p.backoff = map[string]*backoff{}
+	p.clients = map[netip.Addr]*Client{}
+	p.priorities = map[netip.Addr]int{}
+	p.backoff = map[netip.Addr]*backoff{}
 	if p.cfg.ParallelTX {
 		log.Info("Initialise clients with parallel TX feature")
 	}
 	for server, prio := range p.cfg.Servers {
 		// normalize the address
-		ip := net.ParseIP(server)
-		ns := ip.String()
+		ip, err := netip.ParseAddr(server)
+		if err != nil {
+			return fmt.Errorf("parsing server address %q: %w", server, err)
+		}
 		econn := p.eventConn
 		if p.cfg.ParallelTX {
-			conn, err := net.DialUDP("udp", nil, &net.UDPAddr{IP: ip, Port: ptp.PortEvent})
+			conn, err := net.DialUDP("udp", nil, &net.UDPAddr{IP: ip.AsSlice(), Port: ptp.PortEvent})
 			if err != nil {
 				return err
 			}
@@ -104,13 +107,13 @@ func (p *SPTP) initClients() error {
 				return err
 			}
 		}
-		c, err := NewClient(ns, ptp.PortEvent, p.clockID, econn, p.cfg, p.stats)
+		c, err := NewClient(ip, ptp.PortEvent, p.clockID, econn, p.cfg, p.stats)
 		if err != nil {
-			return fmt.Errorf("initializing client %q: %w", ns, err)
+			return fmt.Errorf("initializing client %v: %w", ip, err)
 		}
-		p.clients[ns] = c
-		p.priorities[ns] = prio
-		p.backoff[ns] = newBackoff(p.cfg.Backoff)
+		p.clients[ip] = c
+		p.priorities[ip] = prio
+		p.backoff[ip] = newBackoff(p.cfg.Backoff)
 	}
 	return nil
 }
@@ -195,13 +198,13 @@ func (p *SPTP) init() error {
 
 // ptping probing if packet is ptping before discarding it
 // It's used for external pingers such as ptping and not required for sptp itself
-func (p *SPTP) ptping(sourceIP net.IP, sourcePort int, response []byte, rxtx time.Time) error {
+func (p *SPTP) ptping(sourceIP netip.Addr, sourcePort int, response []byte, rxtx time.Time) error {
 	// Has to be a delay request from ptping
 	b := &ptp.SyncDelayReq{}
 	if err := ptp.FromBytes(response, b); err != nil {
 		return fmt.Errorf("failed to read delay request %w", err)
 	}
-	c, err := NewClient(sourceIP.String(), sourcePort, p.clockID, p.eventConn, p.cfg, p.stats)
+	c, err := NewClient(sourceIP, sourcePort, p.clockID, p.eventConn, p.cfg, p.stats)
 	if err != nil {
 		return fmt.Errorf("failed to respond to a delay request %w", err)
 	}
@@ -227,7 +230,7 @@ func (p *SPTP) RunListener(ctx context.Context) error {
 					doneChan <- err
 					return
 				}
-				if addr == "" {
+				if !addr.IsValid() {
 					doneChan <- fmt.Errorf("received packet on port 320 with nil source address")
 					return
 				}
@@ -270,8 +273,8 @@ func (p *SPTP) RunListener(ctx context.Context) error {
 					return
 				}
 				log.Debugf("got packet on port 319, addr = %v", addr)
-				ip := timestamp.SockaddrToIP(addr)
-				cc, found := p.clients[ip.String()]
+				ip := timestamp.SockaddrToAddr(addr)
+				cc, found := p.clients[ip]
 				if !found {
 					log.Warningf("ignoring packets from server %v. Trying ptping", ip)
 					// Try ptping
@@ -301,7 +304,7 @@ func (p *SPTP) RunListener(ctx context.Context) error {
 	return eg.Wait()
 }
 
-func (p *SPTP) handleExchangeError(addr string, err error, tickDuration time.Duration) {
+func (p *SPTP) handleExchangeError(addr netip.Addr, err error, tickDuration time.Duration) {
 	if errors.Is(err, errBackoff) {
 		b := p.backoff[addr].dec(tickDuration)
 		log.Debugf("backoff %s: %s", addr, b)
@@ -324,7 +327,7 @@ func (p *SPTP) setMeanFreq() float64 {
 }
 
 // reprioritize is pushing former "best gm" to the back of the list
-func (p *SPTP) reprioritize(bestAddr string) {
+func (p *SPTP) reprioritize(bestAddr netip.Addr) {
 	// by how much we should shift the list
 	k := p.priorities[bestAddr] - 1
 	for addr, prio := range p.priorities {
@@ -336,7 +339,7 @@ func (p *SPTP) reprioritize(bestAddr string) {
 	}
 }
 
-func (p *SPTP) processResults(results map[string]*RunResult) {
+func (p *SPTP) processResults(results map[netip.Addr]*RunResult) {
 	defer func() {
 		for addr, res := range results {
 			s := runResultToGMStats(addr, res, p.priorities[addr], addr == p.bestGM)
@@ -360,7 +363,7 @@ func (p *SPTP) processResults(results map[string]*RunResult) {
 	p.lastTick = now
 	gmsTotal := len(results)
 	gmsAvailable := 0
-	idsToClients := map[ptp.ClockIdentity]string{}
+	idsToClients := map[ptp.ClockIdentity]netip.Addr{}
 	localPrioMap := map[ptp.ClockIdentity]int{}
 	for addr, res := range results {
 		if res.Error == nil {
@@ -387,7 +390,7 @@ func (p *SPTP) processResults(results map[string]*RunResult) {
 	best := bmca(results, localPrioMap, p.cfg)
 	if best == nil {
 		log.Warningf("no Best Master selected")
-		p.bestGM = ""
+		p.bestGM = netip.Addr{}
 		freqAdj := p.setMeanFreq()
 		log.Infof("offset Unknown s%d freq %+7.0f path delay Unknown", servo.StateHoldover, freqAdj)
 		return
@@ -442,7 +445,7 @@ func (p *SPTP) runInternal(ctx context.Context) error {
 
 	tick := func() {
 		eg, ictx := errgroup.WithContext(ctx)
-		results := map[string]*RunResult{}
+		results := map[netip.Addr]*RunResult{}
 		for addr, c := range p.clients {
 			addr := addr
 			c := c

ptp/sptp/client/bmca.go
@@ -17,12 +17,14 @@ limitations under the License.
 package client
 
 import (
+	"net/netip"
+
 	ptp "github.com/facebook/time/ptp/protocol"
 
 	"github.com/facebook/time/ptp/sptp/bmc"
 )
 
-func bmca(results map[string]*RunResult, prios map[ptp.ClockIdentity]int, cfg *Config) *ptp.Announce {
+func bmca(results map[netip.Addr]*RunResult, prios map[ptp.ClockIdentity]int, cfg *Config) *ptp.Announce {
 	if len(results) == 0 {
 		return nil
 	}

ptp/sptp/client/bmca_test.go
@@ -17,43 +17,48 @@ limitations under the License.
 package client
 
 import (
+	"net/netip"
+	"testing"
+
 	ptp "github.com/facebook/time/ptp/protocol"
 	"github.com/stretchr/testify/require"
-	"testing"
 )
 
+var best = netip.MustParseAddr("1.1.1.1")
+var worse = netip.MustParseAddr("4.4.4.4")
+
 func TestBmcaProperlyUsesClockQuality(t *testing.T) {
-	results := map[string]*RunResult{
-		"best": {
+	results := map[netip.Addr]*RunResult{
+		best: {
 			Measurement: &MeasurementResult{Announce: ptp.Announce{AnnounceBody: ptp.AnnounceBody{GrandmasterIdentity: 1, GrandmasterClockQuality: ptp.ClockQuality{ClockClass: ptp.ClockClass7}}}},
 		},
-		"worse": {
+		worse: {
 			Measurement: &MeasurementResult{Announce: ptp.Announce{AnnounceBody: ptp.AnnounceBody{GrandmasterIdentity: 2, GrandmasterClockQuality: ptp.ClockQuality{ClockClass: ptp.ClockClass13}}}},
 		},
 	}
 	selected := bmca(results, map[ptp.ClockIdentity]int{1: 2, 2: 1}, DefaultConfig())
-	require.Equal(t, results["best"].Measurement.Announce, *selected)
+	require.Equal(t, results[best].Measurement.Announce, *selected)
 }
 
 func TestBmcaProperlyUsesLocalPriority(t *testing.T) {
-	results := map[string]*RunResult{
-		"best": {
+	results := map[netip.Addr]*RunResult{
+		best: {
 			Measurement: &MeasurementResult{Announce: ptp.Announce{AnnounceBody: ptp.AnnounceBody{GrandmasterIdentity: 1, GrandmasterPriority1: 1}}}, // GrandMasterIdentity is ignored with TelcoDscmp
 		},
-		"worse": {
+		worse: {
 			Measurement: &MeasurementResult{Announce: ptp.Announce{AnnounceBody: ptp.AnnounceBody{GrandmasterIdentity: 2, GrandmasterPriority1: 2}}}, // GrandMasterIdentity is ignored with TelcoDscmp
 		},
 	}
 	selected := bmca(results, map[ptp.ClockIdentity]int{1: 1, 2: 2}, DefaultConfig())
-	require.Equal(t, results["best"].Measurement.Announce, *selected)
+	require.Equal(t, results[best].Measurement.Announce, *selected)
 }
 
 func TestBmcaNoMasterForCalibrating(t *testing.T) {
-	results := map[string]*RunResult{
-		"best": {
+	results := map[netip.Addr]*RunResult{
+		best: {
 			Measurement: &MeasurementResult{Announce: ptp.Announce{AnnounceBody: ptp.AnnounceBody{GrandmasterIdentity: 1, GrandmasterClockQuality: ptp.ClockQuality{ClockClass: ptp.ClockClass13}}}},
 		},
-		"worse": {
+		worse: {
 			Measurement: &MeasurementResult{Announce: ptp.Announce{AnnounceBody: ptp.AnnounceBody{GrandmasterIdentity: 2, GrandmasterClockQuality: ptp.ClockQuality{ClockClass: ptp.ClockClass52}}}},
 		},
 	}
@@ -62,11 +67,11 @@ func TestBmcaNoMasterForCalibrating(t *testing.T) {
 }
 
 func TestBmcaNoMasterForLowAccuracy(t *testing.T) {
-	results := map[string]*RunResult{
-		"best": {
+	results := map[netip.Addr]*RunResult{
+		best: {
 			Measurement: &MeasurementResult{Announce: ptp.Announce{AnnounceBody: ptp.AnnounceBody{GrandmasterIdentity: 1, GrandmasterClockQuality: ptp.ClockQuality{ClockAccuracy: ptp.ClockAccuracyMicrosecond100}}}},
 		},
-		"worse": {
+		worse: {
 			Measurement: &MeasurementResult{Announce: ptp.Announce{AnnounceBody: ptp.AnnounceBody{GrandmasterIdentity: 2, GrandmasterClockQuality: ptp.ClockQuality{ClockAccuracy: ptp.ClockAccuracySecond10}}}},
 		},
 	}

ptp/sptp/client/client.go
@@ -23,6 +23,7 @@ import (
 	"fmt"
 	rnd "math/rand"
 	"net"
+	"net/netip"
 	"time"
 
 	log "github.com/sirupsen/logrus"
@@ -71,14 +72,14 @@ func ReqAnnounce(clockID ptp.ClockIdentity, portID uint16, ts time.Time) *ptp.An
 
 // RunResult is what we return from single client-server interaction
 type RunResult struct {
-	Server      string
+	Server      netip.Addr
 	Measurement *MeasurementResult
 	Error       error
 }
 
 // Client is a part of PTPNG that talks to only one server
 type Client struct {
-	server string
+	server netip.Addr
 	// packet sequence counter
 	eventSequence uint16
 	// mask for sequence ID value
@@ -153,10 +154,10 @@ func (c *Client) SendAnnounce(p *ptp.Announce) (uint16, error) {
 }
 
 // NewClient initializes sptp client
-func NewClient(target string, targetPort int, clockID ptp.ClockIdentity, eventConn UDPConnWithTS, cfg *Config, stats StatsServer) (*Client, error) {
+func NewClient(target netip.Addr, targetPort int, clockID ptp.ClockIdentity, eventConn UDPConnWithTS, cfg *Config, stats StatsServer) (*Client, error) {
 	// addresses
 	// where to send to
-	eventAddr, err := net.ResolveUDPAddr("udp", net.JoinHostPort(target, fmt.Sprintf("%d", targetPort)))
+	eventAddr, err := net.ResolveUDPAddr("udp", net.JoinHostPort(target.String(), fmt.Sprintf("%d", targetPort)))
 	if err != nil {
 		return nil, err
 	}

ptp/sptp/client/client_test.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"encoding/binary"
 	"net"
+	"net/netip"
 	"testing"
 	"time"
 
@@ -80,7 +81,7 @@ func TestClientRun(t *testing.T) {
 		},
 	}
 	statsServer := NewMockStatsServer(ctrl)
-	c, err := NewClient("127.0.0.1", ptp.PortEvent, cid, eventConn, &cfg, statsServer)
+	c, err := NewClient(netip.MustParseAddr("127.0.0.1"), ptp.PortEvent, cid, eventConn, &cfg, statsServer)
 	require.NoError(t, err)
 
 	// put stuff into measurements to make sure it got cleaned before the run
@@ -116,7 +117,7 @@ func TestClientRun(t *testing.T) {
 	runResult := c.RunOnce(ctx, defaultTestTimeout)
 	require.NotNil(t, runResult)
 	require.NoError(t, runResult.Error, "full client run should succeed")
-	require.Equal(t, "127.0.0.1", runResult.Server, "run result should have correct server")
+	require.Equal(t, netip.MustParseAddr("127.0.0.1"), runResult.Server, "run result should have correct server")
 	require.NotNil(t, runResult.Measurement, "run result should have measurements")
 	require.Equal(t, *announce, runResult.Measurement.Announce)
 	require.NotEqual(t, 0, runResult.Measurement.Delay)
@@ -143,7 +144,7 @@ func TestClientTimeout(t *testing.T) {
 		},
 	}
 	statsServer := NewMockStatsServer(ctrl)
-	c, err := NewClient("127.0.0.1", ptp.PortEvent, cid, eventConn, &cfg, statsServer)
+	c, err := NewClient(netip.MustParseAddr("127.0.0.1"), ptp.PortEvent, cid, eventConn, &cfg, statsServer)
 	require.NoError(t, err)
 	statsServer.EXPECT().IncTXDelayReq()
 	eventConn.EXPECT().WriteToWithTS(gomock.Any(), gomock.Any())
@@ -169,7 +170,7 @@ func TestClientBadPacket(t *testing.T) {
 		},
 	}
 	statsServer := NewMockStatsServer(ctrl)
-	c, err := NewClient("127.0.0.1", ptp.PortEvent, cid, eventConn, &cfg, statsServer)
+	c, err := NewClient(netip.MustParseAddr("127.0.0.1"), ptp.PortEvent, cid, eventConn, &cfg, statsServer)
 	require.NoError(t, err)
 
 	// handle whatever client is sending over eventConn
@@ -187,7 +188,7 @@ func TestClientBadPacket(t *testing.T) {
 	runResult := c.RunOnce(ctx, defaultTestTimeout)
 	require.NotNil(t, runResult)
 	require.Error(t, runResult.Error, "full client run should fail")
-	require.Equal(t, "127.0.0.1", runResult.Server, "run result should have correct server")
+	require.Equal(t, netip.MustParseAddr("127.0.0.1"), runResult.Server, "run result should have correct server")
 }
 
 func TestClientIncrementSequence(t *testing.T) {
@@ -207,7 +208,7 @@ func TestClientIncrementSequence(t *testing.T) {
 		SequenceIDMaskValue: 3,
 	}
 	statsServer := NewMockStatsServer(ctrl)
-	c, err := NewClient("127.0.0.1", ptp.PortEvent, cid, eventConn, &cfg, statsServer)
+	c, err := NewClient(netip.MustParseAddr("127.0.0.1"), ptp.PortEvent, cid, eventConn, &cfg, statsServer)
 	require.NoError(t, err)
 	require.Equal(t, uint16(0x3FFF), c.sequenceIDMask)
 	require.Equal(t, uint16(0xC000), c.sequenceIDValue)

ptp/sptp/client/connection.go
@@ -19,6 +19,7 @@ package client
 import (
 	"fmt"
 	"net"
+	"net/netip"
 	"sync"
 	"time"
 
@@ -31,7 +32,7 @@ import (
 // UDPConnNoTS describes what functionality we expect from UDP connection
 type UDPConnNoTS interface {
 	WriteTo(b []byte, addr net.Addr) (int, error)
-	ReadPacketBuf(buf []byte) (int, string, error)
+	ReadPacketBuf(buf []byte) (int, netip.Addr, error)
 	Close() error
 }
 
@@ -141,11 +142,11 @@ func (c *UDPConnTS) ReadPacketWithRXTimestampBuf(buf, oob []byte) (int, unix.Soc
 }
 
 // ReadPacketBuf reads bytes from underlying fd
-func (c *UDPConn) ReadPacketBuf(buf []byte) (int, string, error) {
+func (c *UDPConn) ReadPacketBuf(buf []byte) (int, netip.Addr, error) {
 	n, saddr, err := unix.Recvfrom(c.connFd, buf, 0)
 	if err != nil {
-		return 0, "", err
+		return 0, netip.Addr{}, err
 	}
 
-	return n, timestamp.SockaddrToIP(saddr).String(), err
+	return n, timestamp.SockaddrToAddr(saddr), err
 }

ptp/sptp/client/sptp_test.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"fmt"
 	"net"
+	"net/netip"
 	"testing"
 	"time"
 
@@ -51,7 +52,7 @@ func TestProcessResultsNoResults(t *testing.T) {
 		pi:    mockServo,
 		stats: mockStatsServer,
 	}
-	results := map[string]*RunResult{}
+	results := map[netip.Addr]*RunResult{}
 	mockServo.EXPECT().MeanFreq()
 	mockServo.EXPECT().SetLastFreq(float64(0))
 	mockClock.EXPECT().AdjFreqPPB(gomock.Any())
@@ -59,7 +60,7 @@ func TestProcessResultsNoResults(t *testing.T) {
 	mockStatsServer.EXPECT().SetGmsAvailable(0)
 	p.processResults(results)
 
-	require.Equal(t, "", p.bestGM)
+	require.Equal(t, netip.Addr{}, p.bestGM)
 }
 
 func TestProcessResultsEmptyResult(t *testing.T) {
@@ -81,8 +82,8 @@ func TestProcessResultsEmptyResult(t *testing.T) {
 	}
 	err := p.initClients()
 	require.NoError(t, err)
-	results := map[string]*RunResult{
-		"192.168.0.10": {},
+	results := map[netip.Addr]*RunResult{
+		netip.MustParseAddr("192.168.0.10"): {},
 	}
 	meanFreq := 10.2
 	mockServo.EXPECT().MeanFreq().Return(meanFreq)
@@ -92,7 +93,7 @@ func TestProcessResultsEmptyResult(t *testing.T) {
 	mockStatsServer.EXPECT().SetGmsAvailable(0)
 	mockStatsServer.EXPECT().SetGMStats(gomock.Any())
 	p.processResults(results)
-	require.Equal(t, "", p.bestGM)
+	require.Equal(t, netip.Addr{}, p.bestGM)
 }
 
 func TestProcessResultsSingle(t *testing.T) {
@@ -124,9 +125,9 @@ func TestProcessResultsSingle(t *testing.T) {
 		stats: mockStatsServer,
 		cfg:   cfg,
 	}
-	results := map[string]*RunResult{
-		"192.168.0.10": {
-			Server: "192.168.0.10",
+	results := map[netip.Addr]*RunResult{
+		netip.MustParseAddr("192.168.0.10"): {
+			Server: netip.MustParseAddr("192.168.0.10"),
 			Measurement: &MeasurementResult{
 				Delay:     299995 * time.Microsecond,
 				S2CDelay:  100,
@@ -140,18 +141,18 @@ func TestProcessResultsSingle(t *testing.T) {
 	require.NoError(t, err)
 	// we step here
 	p.processResults(results)
-	require.Equal(t, "192.168.0.10", p.bestGM)
+	require.Equal(t, netip.MustParseAddr("192.168.0.10"), p.bestGM)
 
 	// we have to wait a second to avoid "samples are too fast" error
 	time.Sleep(time.Second)
-	results["192.168.0.10"].Measurement.Offset = -100001 * time.Microsecond
+	results[netip.MustParseAddr("192.168.0.10")].Measurement.Offset = -100001 * time.Microsecond
 	// we adj here
 	mockStatsServer.EXPECT().SetGmsTotal(1)
 	mockStatsServer.EXPECT().SetGmsAvailable(100)
 	mockStatsServer.EXPECT().SetTickDuration(gomock.Any())
 	mockStatsServer.EXPECT().SetGMStats(gomock.Any())
 	p.processResults(results)
-	require.Equal(t, "192.168.0.10", p.bestGM)
+	require.Equal(t, netip.MustParseAddr("192.168.0.10"), p.bestGM)
 }
 
 func TestProcessResultsFastSamples(t *testing.T) {
@@ -183,9 +184,9 @@ func TestProcessResultsFastSamples(t *testing.T) {
 		stats: mockStatsServer,
 		cfg:   cfg,
 	}
-	results := map[string]*RunResult{
-		"192.168.0.10": {
-			Server: "192.168.0.10",
+	results := map[netip.Addr]*RunResult{
+		netip.MustParseAddr("192.168.0.10"): {
+			Server: netip.MustParseAddr("192.168.0.10"),
 			Measurement: &MeasurementResult{
 				Delay:     299995 * time.Microsecond,
 				S2CDelay:  100,
@@ -199,7 +200,7 @@ func TestProcessResultsFastSamples(t *testing.T) {
 	require.NoError(t, err)
 	// we step here
 	p.processResults(results)
-	require.Equal(t, "192.168.0.10", p.bestGM)
+	require.Equal(t, netip.MustParseAddr("192.168.0.10"), p.bestGM)
 
 	// we stick to holdover mode and mean freq
 	mockStatsServer.EXPECT().SetGmsTotal(1)
@@ -207,7 +208,7 @@ func TestProcessResultsFastSamples(t *testing.T) {
 	mockStatsServer.EXPECT().SetTickDuration(gomock.Any())
 	mockStatsServer.EXPECT().SetGMStats(gomock.Any())
 	p.processResults(results)
-	require.Equal(t, "192.168.0.10", p.bestGM)
+	require.Equal(t, netip.MustParseAddr("192.168.0.10"), p.bestGM)
 }
 
 func TestProcessResultsMulti(t *testing.T) {
@@ -249,9 +250,9 @@ func TestProcessResultsMulti(t *testing.T) {
 	announce1 := announcePkt(1)
 	announce1.GrandmasterIdentity = ptp.ClockIdentity(0x042)
 	announce1.GrandmasterPriority2 = 1
-	results := map[string]*RunResult{
-		"192.168.0.10": {
-			Server: "192.168.0.10",
+	results := map[netip.Addr]*RunResult{
+		netip.MustParseAddr("192.168.0.10"): {
+			Server: netip.MustParseAddr("192.168.0.10"),
 			Measurement: &MeasurementResult{
 				Delay:     299995 * time.Microsecond,
 				S2CDelay:  100,
@@ -261,21 +262,21 @@ func TestProcessResultsMulti(t *testing.T) {
 				Announce:  *announce0,
 			},
 		},
-		"192.168.0.11": {
-			Server: "192.168.0.11",
+		netip.MustParseAddr("192.168.0.11"): {
+			Server: netip.MustParseAddr("192.168.0.11"),
 			Error:  fmt.Errorf("context deadline exceeded"),
 		},
 	}
 	// we step here
 	p.processResults(results)
-	require.Equal(t, "192.168.0.10", p.bestGM)
+	require.Equal(t, netip.MustParseAddr("192.168.0.10"), p.bestGM)
 
 	// we have to wait a second to avoid "samples are too fast" error
 	time.Sleep(time.Second)
 
-	results["192.168.0.10"].Measurement.Offset = -100001 * time.Microsecond
-	results["192.168.0.11"].Error = nil
-	results["192.168.0.11"].Measurement = &MeasurementResult{
+	results[netip.MustParseAddr("192.168.0.10")].Measurement.Offset = -100001 * time.Microsecond
+	results[netip.MustParseAddr("192.168.0.11")].Error = nil
+	results[netip.MustParseAddr("192.168.0.11")].Measurement = &MeasurementResult{
 		Delay:     299995 * time.Microsecond,
 		S2CDelay:  90,
 		C2SDelay:  120,
@@ -290,7 +291,7 @@ func TestProcessResultsMulti(t *testing.T) {
 	mockStatsServer.EXPECT().SetGMStats(gomock.Any())
 	mockStatsServer.EXPECT().SetGMStats(gomock.Any())
 	p.processResults(results)
-	require.Equal(t, "192.168.0.11", p.bestGM)
+	require.Equal(t, netip.MustParseAddr("192.168.0.11"), p.bestGM)
 }
 
 func TestRunInternalAllDead(t *testing.T) {
@@ -363,9 +364,9 @@ func TestRunFiltered(t *testing.T) {
 		stats: mockStatsServer,
 		cfg:   cfg,
 	}
-	results := map[string]*RunResult{
-		"192.168.0.10": {
-			Server: "192.168.0.10",
+	results := map[netip.Addr]*RunResult{
+		netip.MustParseAddr("192.168.0.10"): {
+			Server: netip.MustParseAddr("192.168.0.10"),
 			Measurement: &MeasurementResult{
 				Delay:     299995 * time.Microsecond,
 				S2CDelay:  100,
@@ -379,8 +380,8 @@ func TestRunFiltered(t *testing.T) {
 	require.NoError(t, err)
 	// we step here
 	p.processResults(results)
-	require.Equal(t, "192.168.0.10", p.bestGM)
-	require.Nil(t, nil, results["192.168.0.10"])
+	require.Equal(t, netip.MustParseAddr("192.168.0.10"), p.bestGM)
+	require.Nil(t, nil, results[netip.MustParseAddr("192.168.0.10")])
 }
 
 func TestRunListenerNoAddr(t *testing.T) {
@@ -422,7 +423,7 @@ func TestRunListenerError(t *testing.T) {
 	mockEventConn := NewMockUDPConnWithTS(ctrl)
 	mockEventConn.EXPECT().ReadPacketWithRXTimestampBuf(gomock.Any(), gomock.Any()).Return(0, &unix.SockaddrInet6{}, time.Time{}, fmt.Errorf("some error")).AnyTimes()
 	mockGenConn := NewMockUDPConnNoTS(ctrl)
-	mockGenConn.EXPECT().ReadPacketBuf(gomock.Any()).Return(2, "", fmt.Errorf("some error")).AnyTimes()
+	mockGenConn.EXPECT().ReadPacketBuf(gomock.Any()).Return(2, netip.Addr{}, fmt.Errorf("some error")).AnyTimes()
 	mockClock := NewMockClock(ctrl)
 	mockServo := NewMockServo(ctrl)
 	mockStatsServer := NewMockStatsServer(ctrl)
@@ -481,10 +482,10 @@ func TestRunListenerGood(t *testing.T) {
 	}).AnyTimes()
 
 	mockGenConn := NewMockUDPConnNoTS(ctrl)
-	mockGenConn.EXPECT().ReadPacketBuf(gomock.Any()).DoAndReturn(func(b []byte) (int, string, error) {
+	mockGenConn.EXPECT().ReadPacketBuf(gomock.Any()).DoAndReturn(func(b []byte) (int, netip.Addr, error) {
 		// limit how many we send, so we don't overwhelm the client. packets from uknown IPs will be discarded
 		if sentGen > 10 {
-			return 0, "whatever", nil
+			return 0, netip.MustParseAddr("4.4.4.4"), nil
 		}
 		sentGen++
 		addr := "192.168.0.11"
@@ -497,7 +498,7 @@ func TestRunListenerGood(t *testing.T) {
 		b[1] = 3
 		b[2] = 7
 		b[3] = 4
-		return len(announceBytes), addr, nil
+		return len(announceBytes), netip.MustParseAddr(addr), nil
 	}).AnyTimes()
 
 	mockClock := NewMockClock(ctrl)
@@ -535,9 +536,9 @@ LOOP:
 		select {
 		case <-nctx.Done():
 			break LOOP
-		case <-p.clients["192.168.0.10"].inChan:
+		case <-p.clients[netip.MustParseAddr("192.168.0.10")].inChan:
 			received10++
-		case <-p.clients["192.168.0.11"].inChan:
+		case <-p.clients[netip.MustParseAddr("192.168.0.11")].inChan:
 			received11++
 		}
 	}
@@ -564,7 +565,7 @@ func TestPTPing(t *testing.T) {
 	}
 
 	response := []byte{}
-	ip := net.ParseIP("1.2.3.4")
+	ip := netip.MustParseAddr("1.2.3.4")
 
 	err := p.ptping(ip, 1234, response, time.Time{})
 	require.Equal(t, "failed to read delay request not enough data to decode SyncDelayReq", err.Error())
@@ -577,31 +578,35 @@ func TestPTPing(t *testing.T) {
 }
 
 func TestShiftPriorities(t *testing.T) {
+	o := netip.MustParseAddr("1.1.1.1")
+	l := netip.MustParseAddr("1.1.1.2")
+	e := netip.MustParseAddr("1.1.1.3")
+	g := netip.MustParseAddr("1.1.1.4")
 	p := &SPTP{
-		priorities: map[string]int{
-			"O": 1,
-			"L": 2,
-			"E": 3,
-			"G": 4,
+		priorities: map[netip.Addr]int{
+			o: 1,
+			l: 2,
+			e: 3,
+			g: 4,
 		},
 	}
 
-	p.reprioritize("L")
-	require.Equal(t, 1, p.priorities["L"])
-	require.Equal(t, 2, p.priorities["E"])
-	require.Equal(t, 3, p.priorities["G"])
-	require.Equal(t, 4, p.priorities["O"])
+	p.reprioritize(l)
+	require.Equal(t, 1, p.priorities[l])
+	require.Equal(t, 2, p.priorities[e])
+	require.Equal(t, 3, p.priorities[g])
+	require.Equal(t, 4, p.priorities[o])
 
-	p.reprioritize("O")
-	require.Equal(t, 1, p.priorities["O"])
-	require.Equal(t, 2, p.priorities["L"])
-	require.Equal(t, 3, p.priorities["E"])
-	require.Equal(t, 4, p.priorities["G"])
+	p.reprioritize(o)
+	require.Equal(t, 1, p.priorities[o])
+	require.Equal(t, 2, p.priorities[l])
+	require.Equal(t, 3, p.priorities[e])
+	require.Equal(t, 4, p.priorities[g])
 
 	// Shift by 0 (no change)
-	p.reprioritize("O")
-	require.Equal(t, 1, p.priorities["O"])
-	require.Equal(t, 2, p.priorities["L"])
-	require.Equal(t, 3, p.priorities["E"])
-	require.Equal(t, 4, p.priorities["G"])
+	p.reprioritize(o)
+	require.Equal(t, 1, p.priorities[o])
+	require.Equal(t, 2, p.priorities[l])
+	require.Equal(t, 3, p.priorities[e])
+	require.Equal(t, 4, p.priorities[g])
 }

commit 5b83bea5288b93f1187b219eca639c0817395021

fix ability to use server names in sptp/ptping
Summary: This got accidently broken by https://github.com/facebook/time/pull/370, ooops. Very inconvenient with ptping.

Reviewed By: leoleovich

Differential Revision: D61666901

fbshipit-source-id: 0ea58380a6f8a53f37c03c2d7d6d63787a78f440

cmd/ptpcheck/cmd/ptping.go
@@ -176,7 +176,7 @@ func ptpingRun(iface string, dscp int, server string, count int, timeout time.Du
 		dscp:  dscp,
 	}
 
-	p.target, err = netip.ParseAddr(server)
+	p.target, err = client.LookupNetIP(server)
 	if err != nil {
 		return err
 	}

ptp/sptp/client/config.go
@@ -19,6 +19,7 @@ package client
 import (
 	"fmt"
 	"net"
+	"net/netip"
 	"os"
 	"time"
 
@@ -27,6 +28,23 @@ import (
 	log "github.com/sirupsen/logrus"
 	yaml "gopkg.in/yaml.v2"
 )
+
+// LookupNetIP returns netip.Addr from addr string, which can be either IP or hostname
+func LookupNetIP(addr string) (netip.Addr, error) {
+	ip, err := netip.ParseAddr(addr)
+	if err != nil {
+		ips, err := net.LookupIP(addr)
+		if err != nil {
+			return netip.Addr{}, err
+		}
+		if len(ips) == 0 {
+			return netip.Addr{}, fmt.Errorf("no ips found for %s", addr)
+		}
+		ip, _ = netip.AddrFromSlice(ips[0])
+		return ip, nil
+	}
+	return ip, nil
+}
 
 // BackoffConfig describes configuration for backoff in case of unavailable GM
 type BackoffConfig struct {

ptp/sptp/client/sptp.go
@@ -92,7 +92,7 @@ func (p *SPTP) initClients() error {
 	}
 	for server, prio := range p.cfg.Servers {
 		// normalize the address
-		ip, err := netip.ParseAddr(server)
+		ip, err := LookupNetIP(server)
 		if err != nil {
 			return fmt.Errorf("parsing server address %q: %w", server, err)
 		}