93d8b25f57cf583ed7982d36f67b1a42390e3c88
Yaroslav Kolomiiets <yrk@meta.com>
2024-10-07 11:46:59 -0700

    ptpcheck: merge adjfreq subcommand into phc subcommand
    Summary: Make `ptpcheck phc` subcommand to incorporate the functionality of `ptpcheck adjfreq`: `ptpcheck phc -f `*freq* now sets the frequency on device; `-t` steps the clock by a delta. When we do either of these settings, printing of resulting state is suppressed – unless `-p` is also specified.
    
    Reviewed By: leoleovich
    
    Differential Revision: D63980265
    
    fbshipit-source-id: 9e4822ca493bde7686dfe037d02e2ccfcca20f4b

--- cmd/ptpcheck/cmd/phc.go
+++ cmd/ptpcheck/cmd/phc.go
@@ -18,6 +18,9 @@ package cmd
 
 import (
 	"fmt"
+	"math"
+	"os"
+	"time"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -25,20 +28,85 @@ import (
 	"github.com/facebook/time/phc"
 )
 
-// flag
-var device string
-var method string
+// flags
+var (
+	device      string
+	method      string
+	freq        float64
+	step        time.Duration
+	setAndPrint bool
+)
+
+var phcCmd = &cobra.Command{
+	Use:   "phc",
+	Short: "Print PHC clock information. Use `phc_ctl` cli for richer functionality",
+	Run:   runPhcCmd,
+}
 
 func init() {
 	RootCmd.AddCommand(phcCmd)
-	phcCmd.Flags().StringVarP(&device, "device", "d", "/dev/ptp0", "PTP device to get time from")
-	phcCmd.Flags().StringVarP(
-		&method,
-		"method",
-		"m",
-		string(phc.MethodIoctlSysOffsetExtended),
+	flags := phcCmd.Flags()
+	flags.StringVarP(&device, "device", "d", "/dev/ptp0", "PTP device to get time from")
+	flags.StringVarP(&method, "method", "m", string(phc.MethodIoctlSysOffsetExtended),
 		fmt.Sprintf("Method to get PHC time: %v", phc.SupportedMethods),
 	)
+	flags.Float64VarP(&freq, "freq", "f", math.NaN(), "set the frequency (PPB)")
+	flags.DurationVarP(&step, "step", "t", 0, "step the clock")
+	flags.BoolVarP(&setAndPrint, "print", "p", false, "print clock status after changes")
+}
+
+func runPhcCmd(_ *cobra.Command, _ []string) {
+	var doPrint = true
+
+	ConfigureVerbosity()
+	if step != 0 {
+		if err := stepPHC(device, step); err != nil {
+			log.Fatal(err)
+		}
+		doPrint = setAndPrint
+	}
+	if !math.IsNaN(freq) {
+		if err := tunePHC(device, freq); err != nil {
+			log.Fatal(err)
+		}
+		doPrint = setAndPrint
+	}
+	if doPrint {
+		if err := printPHC(device, phc.TimeMethod(method)); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func stepPHC(device string, step time.Duration) error {
+	f, err := os.OpenFile(device, os.O_RDWR, 0)
+	if err != nil {
+		return fmt.Errorf("opening device %q: %w", device, err)
+	}
+	defer f.Close()
+	dev := phc.FromFile(f)
+
+	fmt.Printf("Stepping the clock by %v\n", step)
+	return dev.Step(step)
+}
+
+func tunePHC(device string, freq float64) error {
+	f, err := os.OpenFile(device, os.O_RDWR, 0)
+	if err != nil {
+		return fmt.Errorf("opening device %q: %w", device, err)
+	}
+	defer f.Close()
+	dev := phc.FromFile(f)
+
+	maxFreq, err := dev.MaxFreqAdjPPB()
+	if err != nil {
+		return err
+	}
+	if freq < -maxFreq || freq > maxFreq {
+		return fmt.Errorf("frequncy %f is out supported range", freq)
+	}
+	fmt.Printf("Setting new frequency value %f\n", freq)
+	return dev.AdjFreq(freq)
 }
 
 func printPHC(device string, method phc.TimeMethod) error {
@@ -57,17 +125,23 @@ func printPHC(device string, method phc.TimeMethod) error {
 	fmt.Printf("SYS clock: %s\n", timeAndOffset.SysTime)
 	fmt.Printf("Offset: %s\n", timeAndOffset.Offset)
 	fmt.Printf("Delay: %s\n", timeAndOffset.Delay)
+
+	f, err := os.OpenFile(device, os.O_RDWR, 0)
+	if err != nil {
+		return fmt.Errorf("opening device %q to read frequency: %w", device, err)
+	}
+	defer f.Close()
+	dev := phc.FromFile(f)
+
+	curFreq, err := dev.FreqPPB()
+	if err != nil {
+		return err
+	}
+	maxFreq, err := dev.MaxFreqAdjPPB()
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Current frequency: %f\n", curFreq)
+	fmt.Printf("Frequency range: [%.2f, %.2f]\n", -maxFreq, maxFreq)
 	return nil
-}
-
-var phcCmd = &cobra.Command{
-	Use:   "phc",
-	Short: "Print PHC clock information. Use `phc_ctl` cli for richer functionality",
-	Run: func(_ *cobra.Command, _ []string) {
-		ConfigureVerbosity()
-
-		if err := printPHC(device, phc.TimeMethod(method)); err != nil {
-			log.Fatal(err)
-		}
-	},
 }
