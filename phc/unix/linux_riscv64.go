84cd85fe2604c434cc04a8bc93793b61f7200936
Yaroslav Kolomiiets <yrk@meta.com>
2024-10-22 02:19:11 -0700

    time: import golang.org/cl/621375 (#414)
    Summary:
    Pull Request resolved: https://github.com/facebook/time/pull/414
    
    https://go-review.googlesource.com/c/sys/+/621375: unix: add PTP IOCTLs on Linux
    
    Import into the local shim so we can start adopting the code before the version is cut.
    
    Reviewed By: leoleovich
    
    Differential Revision: D64695439
    
    fbshipit-source-id: 9765b49b0467e4523b2cad77c3a9a1aa07fb5f35

--- phc/unix/linux_riscv64.go
+++ phc/unix/linux_riscv64.go
@@ -1,5 +1,6 @@
 //go:build riscv64 && linux
 
+// @generated
 /*
 Copyright (c) Facebook, Inc. and its affiliates.
 
@@ -22,3 +23,20 @@ type ifreq struct {
 	Ifrn [16]byte
 	Ifru [24]byte
 }
+
+const (
+	PTP_CLOCK_GETCAPS   = 0x80503d01 //nolint:revive
+	PTP_CLOCK_GETCAPS2  = 0x80503d0a //nolint:revive
+	PTP_ENABLE_PPS      = 0x40043d04 //nolint:revive
+	PTP_ENABLE_PPS2     = 0x40043d0d //nolint:revive
+	PTP_EXTTS_REQUEST   = 0x40103d02 //nolint:revive
+	PTP_EXTTS_REQUEST2  = 0x40103d0b //nolint:revive
+	PTP_MASK_CLEAR_ALL  = 0x3d13     //nolint:revive
+	PTP_MASK_EN_SINGLE  = 0x40043d14 //nolint:revive
+	PTP_PEROUT_REQUEST  = 0x40383d03 //nolint:revive
+	PTP_PEROUT_REQUEST2 = 0x40383d0c //nolint:revive
+	PTP_PIN_SETFUNC     = 0x40603d07 //nolint:revive
+	PTP_PIN_SETFUNC2    = 0x40603d10 //nolint:revive
+	PTP_SYS_OFFSET      = 0x43403d05 //nolint:revive
+	PTP_SYS_OFFSET2     = 0x43403d0e //nolint:revive
+)
