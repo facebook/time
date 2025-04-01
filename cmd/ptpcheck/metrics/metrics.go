05cbf1c43ec962835168f86d6dd6e3a87e18d5c6
Cristiano Ruschel Marques Dias <crmdias@meta.com>
2025-04-01 09:23:04 -0700

    Add metrics server and offset metric to ts2phc
    Summary:
    WHAT?
    Adds offset gauge (current offset value) and offset summary (p1 and p99 offset values) for ts2phc
    
    WHY?
    Enable better monitoring
    
    Reviewed By: leoleovich
    
    Differential Revision: D71916841
    
    fbshipit-source-id: e4f8b85c918099e7346b4ce97b83f72bc30ba723

---
+++ cmd/ptpcheck/metrics/metrics.go
@@ -0,0 +1,71 @@
+package metrics
+
+import (
+	"container/list"
+	"encoding/json"
+	"fmt"
+	"math"
+	"net/http"
+	"sync"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+)
+
+// maxSamples is the maximum samples considered for calculating min/max offset
+const maxSamples = 60
+
+var (
+	minOffset, maxOffset = 0.0, 0.0
+	offsets              list.List
+	offsetsLock          sync.Mutex
+)
+
+// RunMetricsServer starts a metrics server on the given port
+func RunMetricsServer(monitoringPort uint) error {
+	log.Infof("Starting HTTP JSON metrics server - query at localhost:%d/metrics", monitoringPort)
+	server := &http.Server{
+		Addr:         fmt.Sprintf(":%d", monitoringPort),
+		ReadTimeout:  time.Second,
+		WriteTimeout: time.Second,
+	}
+	http.Handle("/metrics", &metricsHandler{})
+	return server.ListenAndServe()
+}
+
+// ObserveOffset sets the value of the ts2phc offset metrics
+func ObserveOffset(offset float64) {
+	offsetsLock.Lock()
+	tmpMinOffset, tmpMaxOffset := math.Inf(1), math.Inf(-1)
+	if offsets.Len() >= maxSamples {
+		offsets.Remove(offsets.Back())
+	}
+	offsets.PushFront(offset)
+	for elem := offsets.Front(); elem != nil; elem = elem.Next() {
+		tmpMinOffset = min(tmpMinOffset, elem.Value.(float64))
+		tmpMaxOffset = max(tmpMaxOffset, elem.Value.(float64))
+	}
+	minOffset, maxOffset = tmpMinOffset, tmpMaxOffset
+	offsetsLock.Unlock()
+}
+
+type metricsHandler struct{}
+
+func (h *metricsHandler) ServeHTTP(w http.ResponseWriter, _ *http.Request) {
+	js, err := json.Marshal(getMetrics())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	if _, err = w.Write(js); err != nil {
+		log.Errorf("Failed to reply to metrics request %v", err)
+	}
+}
+
+func getMetrics() map[string]float64 {
+	return map[string]float64{
+		"min_offset": minOffset,
+		"max_offset": maxOffset,
+	}
+}
