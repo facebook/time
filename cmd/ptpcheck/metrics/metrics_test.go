1eec9db1f36c44602999fca67bc7ad4ef40bd3f8
Cristiano Ruschel Marques Dias <crmdias@meta.com>
2025-04-04 08:53:08 -0700

    Export max absolute offset value instead of min and max (#454)
    Summary:
    Pull Request resolved: https://github.com/facebook/time/pull/454
    
    WHAT?
    Title
    
    WHY?
    Make it consistent with other metrics
    
    Reviewed By: t3lurid3
    
    Differential Revision: D72455207
    
    fbshipit-source-id: dec4f00fb3760826ceb4f1c2a749f80f6a0e2b52

--- cmd/ptpcheck/metrics/metrics_test.go
+++ cmd/ptpcheck/metrics/metrics_test.go
@@ -1,85 +1,85 @@
+/*
+Copyright (c) Facebook, Inc. and its affiliates.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package metrics
 
 import (
 	"container/list"
-	"math"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestGetMetrics(t *testing.T) {
-	handler := &Handler{}
-	handler.minOffset = 10.0
-	handler.maxOffset = 20.0
+	handler := &Handler{
+		maxOffsetAbs: 20.0,
+	}
 	metrics := handler.getMetrics()
 	require.Equal(t, map[string]float64{
-		"min_offset": 10.0,
-		"max_offset": 20.0,
+		"offset.abs_max": 20.0,
 	}, metrics)
-	if metrics["min_offset"] != 10.0 {
-		t.Errorf("Expected min_offset to be 10.0, got %f", metrics["min_offset"])
-	}
-	if metrics["max_offset"] != 20.0 {
-		t.Errorf("Expected max_offset to be 20.0, got %f", metrics["max_offset"])
-	}
 }
 
 func TestObserveOffset(t *testing.T) {
 	tests := []struct {
-		name           string
-		offsets        []float64
-		observedOffset float64
-		expectedMin    float64
-		expectedMax    float64
+		name              string
+		offsets           []float64
+		observedOffset    float64
+		expectedOffsetAbs float64
 	}{
 		{
-			name:           "Min offset updated",
-			offsets:        []float64{10.0},
-			observedOffset: -10.0,
-			expectedMin:    -10.0,
-			expectedMax:    10.0,
+			name:              "Max absolute offset updated when given negative value",
+			offsets:           []float64{10.0},
+			observedOffset:    -100.0,
+			expectedOffsetAbs: 100.0,
 		},
 		{
-			name:           "Max offset updated",
-			offsets:        []float64{10.0, 5.0, 15.0},
-			observedOffset: 100.0,
-			expectedMin:    5.0,
-			expectedMax:    100.0,
+			name:              "Max offset updated when given positive value",
+			offsets:           []float64{10.0, 5.0, 15.0, -90.0},
+			observedOffset:    100.0,
+			expectedOffsetAbs: 100.0,
 		},
 		{
-			name:           "None updated",
-			offsets:        []float64{10.0, 5.0, 15.0},
-			observedOffset: 7.0,
-			expectedMin:    5.0,
-			expectedMax:    15.0,
+			name:              "Does not update when |observedOffset| is less than |max| from offsets",
+			offsets:           []float64{10.0, 5.0, 15.0},
+			observedOffset:    7.0,
+			expectedOffsetAbs: 15.0,
 		},
 		{
-			name:           "Exceed max samples (60 samples)",
-			offsets:        append([]float64{-1000}, repeatNumber(maxSamples, 10.0)...),
-			observedOffset: -999.0,
-			expectedMin:    -999.0,
-			expectedMax:    10.0,
+			name:              "Exceed max samples (60 samples)",
+			offsets:           append([]float64{-1000}, repeatNumber(maxSamples, 10.0)...),
+			observedOffset:    -999.0,
+			expectedOffsetAbs: 999.0,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			handler := &Handler{
-				minOffset: math.Inf(1),
-				maxOffset: math.Inf(-1),
-				offsets:   listify(tt.offsets),
+				offsets: listify(tt.offsets),
 			}
 			handler.ObserveOffset(tt.observedOffset)
-			require.Equal(t, tt.expectedMin, handler.minOffset)
-			require.Equal(t, tt.expectedMax, handler.maxOffset)
+			require.Equal(t, tt.expectedOffsetAbs, handler.maxOffsetAbs)
 		})
 	}
 }
 
-// create a slize with the given number repeated repetitionCount times
+// repeatNumber creates a slice with the given number repeated repetitionCount times
 func repeatNumber(repetitionCount int, number float64) []float64 {
 	slice := []float64{}
-	for i := 0; i < repetitionCount; i++ {
+	for range repetitionCount {
 		slice = append(slice, number)
 	}
 	return slice
