d03b856a4cf6780e7f7c58233f694a92d0abdb1f
generatedunixname894542063453888 <noreply+894542063453888@fb.com>
2026-04-14 13:28:56 -0700

    fbcode/time/fbclock/daemon
    Reviewed By: siutsin
    
    Differential Revision: D99795980
    
    fbshipit-source-id: 78920478cf80840bb337c20bd0b8576890eb180d

--- fbclock/daemon/device.go
+++ fbclock/daemon/device.go
@@ -20,13 +20,11 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"time"
 
 	"github.com/facebook/time/fbclock"
 	"github.com/facebook/time/phc"
 
 	log "github.com/sirupsen/logrus"
-	"golang.org/x/sys/unix"
 )
 
 // ManagedPTPDevicePath is the path we will set up a copy of iface's PHC device,
@@ -70,13 +68,4 @@ func SetupDeviceDir(iface string) error {
 		return fmt.Errorf("linking device %s to %s: %w", device, target, err)
 	}
 	return os.Chmod(target, wantMode)
-}
-
-// TimeMonotonicRaw returns the current time from CLOCK_MONOTONIC_RAW
-func TimeMonotonicRaw() (time.Time, error) {
-	var ts unix.Timespec
-	if err := unix.ClockGettime(unix.CLOCK_MONOTONIC_RAW, &ts); err != nil {
-		return time.Time{}, fmt.Errorf("failed clock_gettime: %w", err)
-	}
-	return time.Unix(ts.Unix()), nil
 }
