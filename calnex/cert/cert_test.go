013e4bf73e98f018a7507ae903627d43d94fd4d1
Vadim Fedorenko <vadfed@meta.com>
2026-07-03 06:21:55 -0700

    upgrade test certifiactes
    Summary:
    Tests for calnex/cert contained self-signed test certificate which was generated for 2 years. it's now expired, needs re-creation.
    Make it valid till 2046 to be sure not touch it again
    
    Differential Revision: D110599125

--- calnex/cert/cert_test.go
+++ calnex/cert/cert_test.go
@@ -27,35 +27,38 @@ import (
 // generated with
 // openssl genrsa -out "root.key" 3072
 //
-//	openssl req -x509 -nodes -sha256 -new -key "root.key" -out "root.crt" -days 731 \
-//	  -subj "/CN=Custom Root" \
-//	  -addext "keyUsage = critical, keyCertSign" \
-//	  -addext "basicConstraints = critical, CA:TRUE, pathlen:0" \
-//	  -addext "subjectKeyIdentifier = hash"
+//		openssl req -x509 -nodes -sha256 -new -key "root.key" -out "root.crt" -days 7310 \
+//		  -subj "/CN=Custom Root" \
+//		  -addext "keyUsage = critical, keyCertSign" \
+//		  -addext "basicConstraints = critical, CA:TRUE, pathlen:0" \
+//		  -addext "subjectKeyIdentifier = hash"
+//	   Validity
+//	           Not Before: Jul  3 12:49:00 2026 GMT
+//	           Not After : Jul  8 12:49:00 2046 GMT
 const rootCA = `-----BEGIN CERTIFICATE-----
-MIIEIDCCAoigAwIBAgIUH6gyij3LwI01yP1WRsITIm4op7YwDQYJKoZIhvcNAQEL
-BQAwFjEUMBIGA1UEAwwLQ3VzdG9tIFJvb3QwHhcNMjQwNzAxMTMyOTQ2WhcNMjYw
-NzAyMTMyOTQ2WjAWMRQwEgYDVQQDDAtDdXN0b20gUm9vdDCCAaIwDQYJKoZIhvcN
-AQEBBQADggGPADCCAYoCggGBAL7TbX59+OpBLmzs3orQbBcP1uN9qzaoMRBaJuVj
-f1hn8jhdip2pNnZTyTmVgNDvur3v5AzaQQK1TxHyCbrmoe4I6/tV5YShlXmFx24S
-gXGyVfzshnnNSoIup9knT18VTbYUFfA8CAVIafTlNB20fN+YDv7Ah/SjQ+ZZn+IO
-m+1i9yeJ0aVwzSIvz2re3ei9OoN78aQXoF3Xk833XWiftCCGxZtbTP94sMGn4WUU
-4SmJMaQEIEXD3gLaP+CMOAdEw2FJdJ1OQV5IShaOAq2qhKAwCNWMt3WUaYAWeOhf
-sGGyzWgdsoQ9cze/l3lMk6mHhSOh/nktaUPntSJaGucZjLKtfu6vXn04sepdpsSV
-HgKEGXXcPMN9qStlaJf010pnuKYU556K7IhGCEHPQ/WpxaN468cDZOxpE0XgsMe7
-llvmqhFxvMVvnFpTcbjmW+DgdhaHG9kicVmi012b7AdQ7H6Z3X2psSMgzONhIRcV
-1S38tVTMowTVrHE6fIDapGTkjwIDAQABo2YwZDAfBgNVHSMEGDAWgBQ717pUZqNd
-eylniXMuPxwOL1gvUDAOBgNVHQ8BAf8EBAMCAgQwEgYDVR0TAQH/BAgwBgEB/wIB
-ADAdBgNVHQ4EFgQUO9e6VGajXXspZ4lzLj8cDi9YL1AwDQYJKoZIhvcNAQELBQAD
-ggGBAKOIW9W0slLA8Ib3lU/WdwhDaSWQe0VdDFHnMB77i59Xf5j/npSMEwna2N1T
-1IfLiFEpf6WXSounPJH2+Sy7+030MrthSqCrGriuxV3c3VE6/tFW4WsQ9hu5z8V9
-ZuaS5W96jBaIViDPDybvZ+kLurRL+c0x23EqxrxAy+8HcO+QScREf1nGB2EYNv+n
-kEaPrj/XPXeh7y2oSutrEr9QzREbXSJj2eeNKR4LrlqOd4Z71UTYAktfVY+EM0uB
-8PGqihkTU2uNdPKUD6nBOdFM0OfZSL/mOtsBFkpz+eM+aMDX21hE7zxZ+ap5870X
-s47ClO3BDs16Rvpcby0UmT7bt+mkwuRxAm6RcZScYD6Ym7wyAS+YGb/WOwXZUUL7
-eP6DjPCyn1NBPBhssB9Ll5xZ+ALfQiaOiufkkO2bGlqmCt9Tlvg5alPL42m3HMNh
-q/rA1ZVNsR6wtE/GSbW2vCtF07P24HQBsfKm1ZII0MeZKWlfEyLSKZ87w84wZvhv
-pZKEFw==
+MIIEIDCCAoigAwIBAgIUKHP7CqmIKOtIBsKReRXaaWEaUwEwDQYJKoZIhvcNAQEL
+BQAwFjEUMBIGA1UEAwwLQ3VzdG9tIFJvb3QwHhcNMjYwNzAzMTI0OTAwWhcNNDYw
+NzA4MTI0OTAwWjAWMRQwEgYDVQQDDAtDdXN0b20gUm9vdDCCAaIwDQYJKoZIhvcN
+AQEBBQADggGPADCCAYoCggGBAMmooOlJnVGLTCdo7SWkyDRTeWmxis8ns8wTEiQ6
+1/IlSu1R6+wM5J/KYHcWXsAOjBBOj00GEQ/TeMwjLhOOZU0Y2zCDduvmWsKeOMjz
+UgvuA7frGL/6ruPG9Ko4a2omS8IfAU/OnWARBM5d8wslVIPsirFY6v892qj4i3j3
+OMFMWrDyiCiu+/nltYy2r+Ix2cmIHgddlHDbS7tzZEmlad48VftfAApWQohRVRnb
+FMpBVhuVPfTNjlKLrOsyJi2YqAUMGk3C/KBXXg27OQ+FfH+1vtwzDBjqsSD8EvIh
+kugz71ZcVX9RmnZnaaGlHRM4gaC26t7G2d2yLadJwjBEjStk3Yd1vwnDejKbzd2P
+dux1zWr7n/YF13G/FuNsud7dabNSuh7KjhyPp7SgM9asFd2hsnf6yYW1KwfYRLJi
+LqYuM/qKBjq/gPudii6TptYnnhyruBsAt8umsUM7h+AHYWsrTFZhU2AqmY4rFNi2
+CAM1R6EORsi5RG8ij3N33iXgnwIDAQABo2YwZDAfBgNVHSMEGDAWgBQ0j7rdTIqB
+YIkkfFCTcBcPtjqhEzAOBgNVHQ8BAf8EBAMCAgQwEgYDVR0TAQH/BAgwBgEB/wIB
+ADAdBgNVHQ4EFgQUNI+63UyKgWCJJHxQk3AXD7Y6oRMwDQYJKoZIhvcNAQELBQAD
+ggGBAC4ELWWd/W88S0f0kRworl78b8AQcK+DUkNTFw05kV3+zY0m3mdPHpkZB8C2
+uAcKDlpazMuc6o2Gz+POQ6jDVRqf26wt7lRLghxfG6AbVrITJfI42OjwMxPD8rgy
+ofUx9/jKZiU7btzfyluRbh5lwvVw0/PmKzwltT/7FV1+0EY8+TKLVbzyzq7BOyyG
+OtwnGEcasVT5BmHO7FOv6LDtHxaKrVvoNaYsXC1svc8EugLFaImCSDXqlCF16Ggp
+5gVJZAsEccGyLBkTf0mIOR4Kw85UB3LmEIenfoyT6LGOg7KV+ndc1Py2i2k+t83J
+YK2Z+uEl16x5eRNKHLYq1QmiXedbUsjCK+7w6AoTqp08Ny5vRTe8sOpTRmr3UpBG
+IG00q0bXI0adHBRtxLN+2pM4bxqo6rjSZSXTkFyLYhMCF+zA5RUxjdWX8+1fAckI
+zDydQVA4wWgcP43ZsW9S+wvbxEevfPC787j+WcT4uC8QNTiMDdesJ9bBJFEumbRg
+1qBRBQ==
 -----END CERTIFICATE-----
 `
 
@@ -66,73 +69,74 @@ pZKEFw==
 // -subj "/CN=*.testhost.invalid" \
 // -reqexts SAN -config <(echo "[SAN]\nsubjectAltName=DNS:testhost.invalid,DNS:*.testhost.invalid\n")
 //
-// openssl x509 -req -sha256 -in "testhost.invalid.csr" -out "testhost.invalid.crt" -days 731 \
-//   -CAkey "root.key" -CA "root.crt" -CAcreateserial -extfile <(cat <<END
-//     subjectAltName = DNS:testhost.invalid,DNS:*.testhost.invalid
-//     keyUsage = critical, digitalSignature, keyEncipherment
-//     extendedKeyUsage = serverAuth
-//     basicConstraints = CA:FALSE
-//     authorityKeyIdentifier = keyid:always
-//     subjectKeyIdentifier = none
+//	openssl x509 -req -sha256 -in "testhost.invalid.csr" -out "testhost.invalid.crt" -days 7300 \
+//	  -CAkey "root.key" -CA "root.crt" -CAcreateserial -extfile <(cat <<END
+//	    subjectAltName = DNS:testhost.invalid,DNS:*.testhost.invalid
+//	    keyUsage = critical, digitalSignature, keyEncipherment
+//	    extendedKeyUsage = serverAuth
+//	    basicConstraints = CA:FALSE
+//	    authorityKeyIdentifier = keyid:always
+//	    subjectKeyIdentifier = none
+//
 // END
 // )
-// Validity
-// Not Before: Jul  1 13:29:47 2024 GMT
-// Not After : Jul  2 13:29:47 2026 GMT
-
+//
+//	Validity
+//	    Not Before: Jul  3 13:09:23 2026 GMT
+//	    Not After : Jun 28 13:09:23 2046 GMT
 const testCertificate = `-----BEGIN CERTIFICATE-----
-MIID3zCCAkegAwIBAgIUBATsW3bbqNNSzh4TU+FiOHl8QQUwDQYJKoZIhvcNAQEL
-BQAwFjEUMBIGA1UEAwwLQ3VzdG9tIFJvb3QwHhcNMjQwNzAxMTMyOTQ3WhcNMjYw
-NzAyMTMyOTQ3WjA1MRswGQYDVQQDDBIqLnRlc3Rob3N0LmludmFsaWQxFjAUBgNV
-BAoMDU1ldGEgSUROIFRlc3QwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIB
-AQDN9nk7srvl66/a1VUQ7A6yyYQdnKaKOMWHfWZS2Sl+ZDJqiCAm+LYX3b1ashG6
-QM80TC/bKEqQujSFSaPAqQXj/9v3ETMibYDPbjk8iCAx7VEEy67aJ+ng93raGpUm
-yFJOF+h0sHpbbqmyIM4PVlhUMmshUkTKZyQNWjicEp7wxpH1l7xO0ShRg2EtZnrr
-hK6ukYzlTfAaAHWeUslVV6ppuZy7gobXI7dNyWIk/9CGjF/iqyNuGf/DrExPY2kE
-v9LX84sLsYqn1W53u3QftMSWCTcqTAW5KS93RM5FSHAntBPGNXNGVrQJ1XOfpQhp
-bviM4c5BgOGNJl88RONuOqo/AgMBAAGjgYUwgYIwLwYDVR0RBCgwJoIQdGVzdGhv
-c3QuaW52YWxpZIISKi50ZXN0aG9zdC5pbnZhbGlkMA4GA1UdDwEB/wQEAwIFoDAT
-BgNVHSUEDDAKBggrBgEFBQcDATAJBgNVHRMEAjAAMB8GA1UdIwQYMBaAFDvXulRm
-o117KWeJcy4/HA4vWC9QMA0GCSqGSIb3DQEBCwUAA4IBgQBY8QiImao9+CXt4Rwl
-TgnI2n8E7FiEg/nQhTOvgWqQesTeWNz5ctyqU+XZC5R1Nb0vEiKrC1RSLy7tZ7E3
-zpVdAb6lhKKkpgW7bwYBx5fu4JoqAzlD8flibEr8/jlasRUYlT4nmpflmG9CMwjV
-7PS+E2Vy2uBEJfZBUIRmUECRkRsNiLx8jNLpfAClIO1qzyIwLz66PjPtwwKBa/uA
-gWnsc4uHNeMV2YNO8Sg6ULV3infFnrG3LSJLXGCP+O6HG3Da+kwNMaskJfXhP9q7
-gdzmC+qaoLkpv5DNBh9LQX0QKi1zXNHammZJ9LhafRkpzyt9z03b3gaA8HaLSTsR
-3aBpI7SnM6gLdhhMZf4q7jPvEOa/zenHPtA7D4n086C46Aao5xe938m9x8+0lmnn
-1LjCIc7ze3M42pLhwJUjtzX0W5PBk9UphbsQObO30DN3AAMpEAugQ5W334Goz41u
-r1GkDF/44gf4QxzJOGZuWPoKBr1Bcqkbn7C4BuJ2mbC68MU=
+MIIDxzCCAi+gAwIBAgIUTiSlTNXHhhTXgZ3tUqrNXHWkX+EwDQYJKoZIhvcNAQEL
+BQAwFjEUMBIGA1UEAwwLQ3VzdG9tIFJvb3QwHhcNMjYwNzAzMTMwOTIzWhcNNDYw
+NjI4MTMwOTIzWjAdMRswGQYDVQQDDBIqLnRlc3Rob3N0LmludmFsaWQwggEiMA0G
+CSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDiEBmt/VTTgM7WoPy6dDBZb1FoCrDk
+Gzx2aWF/CVkgPnMM8Keiqf71j2/+ptqJsrjePamDCU5TO7K80IZABggveOOE3uhy
+B3u1Ey+owZvKdwS3CVQrW5JXGLaLtcaiIM4VfuIND60J28GqRVrhZnWb0r/Y6zUi
+0pr6jTlvvF8BKzWiiRtR3LtkhES90IzQKCqM6ssRLFZsg8sr7CYCB+Kr5hjD6kHv
+L9+9IsSbTxSLW3v6SfZd9K+q4fNyWmoevgdDFlJeER62mXaAGdBsaCjmas4q7xZs
+fyvs8DN0jbpRqP60XmpjmPLcF30snNdNUa6ecnKUgpt9wog5peQiOZZnAgMBAAGj
+gYUwgYIwLwYDVR0RBCgwJoIQdGVzdGhvc3QuaW52YWxpZIISKi50ZXN0aG9zdC5p
+bnZhbGlkMA4GA1UdDwEB/wQEAwIFoDATBgNVHSUEDDAKBggrBgEFBQcDATAJBgNV
+HRMEAjAAMB8GA1UdIwQYMBaAFDSPut1MioFgiSR8UJNwFw+2OqETMA0GCSqGSIb3
+DQEBCwUAA4IBgQCOU0q/lYJHyy/mZOGgxhUvn//LsXhFJSHH8/6vDiPsoqNnCKl7
+SW/VURpKgd3jZ33gGGH0Pb/upZNQexfyIQLRzu5buvynMNtDG/bm0xe80hqgIbe4
+VrnVuDnOnHb5FpGKkIsSZY8YzD8baJfZ1AC2h/4ouvvxVTPnpJzsYMQ+f/r5nvBZ
+z3A1FwM088qURuStSwwqExjayGMl5O66oxDwnEvVfP0HLJVhzVpOeqbwpkJZw+37
+7XUbOR+wc8tSugc1qAr5awgt1s8uRlxdEjdA/SQ69jRewcoGbu+qt1W2B/gWZmxA
+AsWMJIMmn5ODfEUpFX5XutII2/e+eP5KDdSrzJ31hGuje3o5ZgZ/UXpp1RKEcr0L
+7N9LUHPOIIaJvYgO2G5k4Ls+6+CwpZiRW17blXlG1Ad/BzHndxREMxpe+8hLvxV3
+ZncHqoFxHThMDOJxv6/EPLZptG9HVUsjQh0KhRCwWEC++fyMLzXUiO17uElZt0GE
+kEhmJtPVJj8bhb0=
 -----END CERTIFICATE-----
 `
 
 // @lint-ignore PRIVATEKEY insecure-private-key-storage
 const testKey = `-----BEGIN PRIVATE KEY-----
-MIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDN9nk7srvl66/a
-1VUQ7A6yyYQdnKaKOMWHfWZS2Sl+ZDJqiCAm+LYX3b1ashG6QM80TC/bKEqQujSF
-SaPAqQXj/9v3ETMibYDPbjk8iCAx7VEEy67aJ+ng93raGpUmyFJOF+h0sHpbbqmy
-IM4PVlhUMmshUkTKZyQNWjicEp7wxpH1l7xO0ShRg2EtZnrrhK6ukYzlTfAaAHWe
-UslVV6ppuZy7gobXI7dNyWIk/9CGjF/iqyNuGf/DrExPY2kEv9LX84sLsYqn1W53
-u3QftMSWCTcqTAW5KS93RM5FSHAntBPGNXNGVrQJ1XOfpQhpbviM4c5BgOGNJl88
-RONuOqo/AgMBAAECggEAXpBNtVUo5DXENgtA1VYsoXXYjOgBpvDN8Jlow50lafyD
-EVqSuJH0uRx79gpQDV34RKC+UDc9lRmJR7E52BlCtR4iVlu1SJdSTuriqKIvdfzp
-9/O0wkEVJs85vq350Sakc2qSthDY/OXgUAKz2WLhhzbm7ROitfOJIABOgYojI5SV
-E4aPDrxCGFMzyBnGhS2Q9m6nrT4PmbnnbIT2nZzBT9FYWmSC9FKmej5IS3AKGLwB
-9ZpuWV1iqZOIgvDhRWntVr24Gn0O5EMTF7RynTV2QlbWuE8zCD1kSjnuicxRGAqf
-OeMbeClkU9jOsVeyJmAzIlwAfs3YbFEz1yMKF4D90QKBgQDa/yLaJmMx/7sxNt+4
-qATWjGMJO8SYggrqeFqqJU+gMGInsyGN6UlLko+kaSQF4kBbceDHb6o2XXlPtK3P
-JFuEQQiV/CbztinyB0UNuJpB06dtfEW22O1cJqrgS5M/xwj1vIE4CRHZKQYzZQ9C
-iXnnfXp0AiOZ2IuaXImrZoCwcwKBgQDww4yHudV2DP5qTGhy4LBfIq2rzY+QDaBa
-g5pWzomJt5eJWgMruwxFEHNIjLSXb2lYq9TTCXKyNzwBF6kGVOM4WCwV+/JOGx7F
-vGg5KUBKPC/9uIYikm9Gs3XMhmcUZqBV7g0B4soiWalwBkI7BtoeObqaSZZ+QZUI
-jyemIjXoBQKBgQCltWTz2RQ6Ix3MEY+btFdk2PmfZQBPvibwYH2KPY1Q0wuSqrL7
-JMj3TEEw0PYXFapJB5RklJQhav1+WGMkWIh/PI54n0ICK5b1spaH2WWv5a3M5LoD
-r4V7sy6dZdJX8g1PlIHamtJMlgRBI3k2ibwadBIScgPqR7bq6JargXZjDQKBgQCU
-avemc5hzPW9Yd+Grb3dKLkaBMibd1oiTQ61Q9eEzVEnGEgcCXjwiFxH6F0L8V2HJ
-l6OKtLhPxFzpD3zSumGXykLjCn1ESNOfcZWOJy/Kk2/CKI4Hod2W5+omOnQwz1Ln
-pee+0d9pbXxV4oXRfVfYah3uHo73JdaJgDYg49X3QQKBgQCan9EfdksF8OVi+5PP
-7gktyg+giMFWnNFFE/Gi9VbyHRSQB/pU9LpdwzlPUSe6QQuKdL93kgWXYgVf3cwF
-zKvIA65gXxPrpkPwWeuzjMIA9pwW9IJudsof6kaZ7NK+T6gBbt91WLVi+zWwoxci
-p89kXDk+P1McDjmisyRqgRV49A==
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDiEBmt/VTTgM7W
+oPy6dDBZb1FoCrDkGzx2aWF/CVkgPnMM8Keiqf71j2/+ptqJsrjePamDCU5TO7K8
+0IZABggveOOE3uhyB3u1Ey+owZvKdwS3CVQrW5JXGLaLtcaiIM4VfuIND60J28Gq
+RVrhZnWb0r/Y6zUi0pr6jTlvvF8BKzWiiRtR3LtkhES90IzQKCqM6ssRLFZsg8sr
+7CYCB+Kr5hjD6kHvL9+9IsSbTxSLW3v6SfZd9K+q4fNyWmoevgdDFlJeER62mXaA
+GdBsaCjmas4q7xZsfyvs8DN0jbpRqP60XmpjmPLcF30snNdNUa6ecnKUgpt9wog5
+peQiOZZnAgMBAAECggEAS8AaYsd/S6ofZSMn3LC/XNCk5iii8qS/w2v3fBKdV2Ul
+t0HS4np1UUKhxCKUG00Ujn/6E8skAFcCQyvauIxs5L9s+eKZ4E/qn5gQwcsykYsF
+PPI2zpqONHo2/STJrR0yAVj1lWvZz3JgeFZqKBplsXPSznSuZv9MaEW3Z94mtaRs
+R2+fBscFruiilseceBIOdH7K9tJ+3qG1qNGCoiQWVN01b2KQsTIeyH/hxoGCh5HD
+tfKZ/E7fVsPvAx56qfiklKbRjOwq7Fy+OzokY7cwBhHVycv/U47qZYYmUpjE72qQ
+9XvVLVlEP+bMY710pTZiGRz84cYfqHz1YE7Fo6MT9QKBgQDypmvODXHrIQ/w9W/F
++LLm8onyepQG2losP/Y0fqTz/+UnoX9EuwxhgsvdPQqyeH5i/DyhyvKJPqNtXGcm
+0rYFOmWCA5CupD/SJSRdXHzk5VZnGU7/hwVTobicJY6AYkPKyl+hCFZ58Y4WwbIN
+v3jh4FI29QqE+Am8IKE3oDBuRQKBgQDugA8HJNxBxvZjQj8zaONp3hS678SVPr91
+0tfVS4QsZFoGAvXxcDvzSIBLrjie15yuloUfGnHKT6VNiprZxRmxAyeWcu1Rlmbs
+pfsUHXy4+fD11A9SZ/54b8Sfbku1neqpq2vcqigpIbXLIWD/243W+lK7e59zGnA8
+7nAyDhKCuwKBgQC7dfTdaKe01oMhTgx/LsbQA1qteSO5M6Hsg7GrFphLZUvdVTgk
+mjlTcCAdmNYV0V8bC/GvsUG05C6QA44xgSJcYaQgUK7LLVuc91Ljyds3XzJkTjoo
+0WA9HzincaBo8QGcvsIof2+HoCV80UHEu0MhhhMeICtzVMj4jWDfv6MK3QKBgAQ4
+sAtoU522b9YB7ixyxtOw4p0McWZS3gCv4rIbzBMdE5rXopLLccQ0nFC6nLXzCwrs
+Dx8l0K3MCxj8QxFns7S2YZUAI8M17kxyA6evfe2oPuObBUpoHND06X4I7b4hNW4b
+YqVdPai8uAMIbDcbI7+SXrSC06et6B6r+cBpD1rRAoGATP7AdyFkeRB51gFdRp8y
+U7/7WDwHtjg8wHf1KbkbXJ8mjyOWrRbT2Rn1SEBOHvYMSZY3189tyQcZH4nKwHGv
+xeCrDMNL1M4RShGpZrcJo1nx60rVL4wX1pSXyOWAoyKGyTl8Yjs5fd6C0VOU51k2
+Vqlm3rrOLjJVbS2Qwx42h/Q=
 -----END PRIVATE KEY-----
 `
 
@@ -201,7 +205,7 @@ func TestBadVerify(t *testing.T) {
 	require.Equal(t, ErrCertNotYetValid, err)
 
 	// Expired
-	testTime, _ = time.Parse(time.RFC3339, "2027-02-03T12:00:00Z")
+	testTime, _ = time.Parse(time.RFC3339, "2047-02-03T12:00:00Z")
 	err = bundle.Verify("testhost.invalid", x509.VerifyOptions{CurrentTime: testTime})
 	require.Equal(t, ErrCertExpired, err)
 }
